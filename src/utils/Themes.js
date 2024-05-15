6b1166bd2eafc7b9e37651e5acc67643230b7dc4
Akhil-Jasson <114808672+Akhil-Jasson@users.noreply.github.com>
2024-05-15 06:32:02 +0530

    Update Themes.js

--- src/utils/Themes.js
+++ src/utils/Themes.js
@@ -1,12 +1,11 @@
 export const darkTheme = {
-    bg:"#1C1C27", // Full bg below hero and navbar //Gradient and little lighter that hero and nav
+    bg:"#1C1C27", 
     bgLight: "#1C1E27",
-    primary:"#FFDF00",
-    // primary:"#854CE6", //Github Profile, textloop, skills in projects, project select //lighter 
-    text_primary:"#F2F3F4", //Headings and navbar links 
-    text_secondary:"#b1b2b3", //I am a, description of all topics, title of projects, title of skills //lighter than primary
-    card:"#171721", //bg of project card and skills card
-    card_light: '#191924', // Heropage bg and NavBar bg
+    primary:"#FFDF00", 
+    text_primary:"#F2F3F4",
+    text_secondary:"#b1b2b3", 
+    card:"#171721", 
+    card_light: '#191924', 
     button:"#854CE6",
     white:"#FFFFFF",
     black:"#000000",
